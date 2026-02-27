d7a60947c87a2a7326d8741949b38b8807065645
Satyaki Das <satyaki1@MacBook-Pro.local>
2026-02-27 22:11:44 +0530

    Change enum

--- ParkingLot/src/Billing.java
+++ ParkingLot/src/Billing.java
@@ -1,23 +1,5 @@
 public class Billing {
     public double calculatePrice(Ticket ticket, int exitTime) {
-        return (exitTime - ticket.getEntryTime()) * ratePerHour(ticket);
-    }
-
-    public double ratePerHour(Ticket ticket) {
-        Vehicle vehicle = ticket.getVehicle();
-        VehicleType vehicleType = vehicle.getVehicleType();
-
-        double rate = 0.0;
-
-        switch(vehicleType) {
-            case TWO_WHEELER -> {
-                rate = 10.0;
-            }
-
-            case FOUR_WHEELER -> {
-                rate = 20.0;
-            }
-        }
-        return rate;
+        return (exitTime - ticket.getEntryTime()) * ticket.getVehicle().getVehicleType().getRate();
     }
 }
--- ParkingLot/src/VehicleType.java
+++ ParkingLot/src/VehicleType.java
@@ -1,3 +1,13 @@
 public enum VehicleType {
-    TWO_WHEELER, FOUR_WHEELER;
+    TWO_WHEELER(10.0),
+    FOUR_WHEELER(20.0);
+
+    final double rate;
+    VehicleType(double rate) {
+        this.rate = rate;
+    }
+
+    public double getRate() {
+        return rate;
+    }
 }
